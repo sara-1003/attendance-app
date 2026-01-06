37e344732fe142bda75a64723cf904df8fd96d3d
sara <ruukuuwan@gmail.com>
2026-01-07 08:26:31 +0900

    マイグレーション実行

---
+++ src/database/migrations/2026_01_07_074251_create_attendance_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->time('new_clock_in')->nullable();
+            $table->time('new_clock_out')->nullable();
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_requests');
+    }
+}
