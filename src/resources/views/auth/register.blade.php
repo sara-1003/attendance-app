7ed04e84f2704becb69d09cf987c62cd754e93d7
sara <ruukuuwan@gmail.com>
2026-01-08 08:11:45 +0900

    親レイアウト作成

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
+@endsection
+
+@section('content')
+
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COACHTECH</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">
+                <img src="{{ asset('images/header.png') }}" alt="COACHTECH">
+            </a>
+            @auth
+                <nav class="header-nav">
+                    <ul class="header-nav__list">
+                        <!-- 管理者ログインの場合 -->
+                        @if(auth()->user()->role === 'admin')
+                            <li class="header-nav__item"><a href="/admin/attendance/list">勤怠一覧</a></li>
+                            <li class="header-nav__item"><a href="/admin/staff/list">スタッフ一覧</a></li>
+                            <li class="header-nav__item"><a href="/stamp_correction_request/list">申請一覧</a></li>
+                            <li class="header-nav__item">
+                                <form action="/logout" method="post">
+                                @csrf
+                                    <button type="submit">ログアウト</button>
+                                </form>
+                            </li>
+                        @else
+                        <!-- 一般ログインの場合 -->
+                            <li class="header-nav__item"><a href="/attendance">勤怠</a></li>
+                            <li class="header-nav__item"><a href="/attendance/list">勤怠一覧</a></li>
+                            <li class="header-nav__item"><a href="/stamp_correction_request/list">申請</a></li>
+                            <li class="header-nav__item">
+                                <form action="/logout" method="post">
+                                @csrf
+                                    <button type="submit">ログアウト</button>
+                                </form>
+                            </li>
+                        @endif
+                    </ul>
+                </nav>
+            @endauth
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
