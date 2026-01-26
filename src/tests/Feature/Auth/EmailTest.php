d44d2d2a60116691ffc4aa5b68e0ac6c8382ec96
sara <ruukuuwan@gmail.com>
2026-01-27 08:03:14 +0900

    テスト実装

--- src/tests/Feature/Auth/EmailTest.php
+++ src/tests/Feature/Auth/EmailTest.php
@@ -2,21 +2,69 @@
 
 namespace Tests\Feature\Auth;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use App\Models\User;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
 
 class EmailTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
+    use RefreshDatabase;
+
+    public function test_会員登録後、認証メールが送信される()
+    {
+        Notification::fake();
+
+        $response = $this->post('/register', [
+            'name' => '太郎',
+            'email' => 'test@example.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ]);
+
+        $user = User::where('email', 'test@example.com')->first();
+
+        Notification::assertSentTo(
+            $user,
+            VerifyEmail::class
+        );
+
+        $this->assertFalse($user->hasVerifiedEmail());
+    }
+
+    public function test_認証はこちらからボタンでメール認証サイトに遷移する()
+    {
+        $user = User::factory()->unverified()->create();
+
+        $response = $this->actingAs($user)->get('/email/verify');
         $response->assertStatus(200);
+
+        $response = $this->actingAs($user)->get('/email/verify/link');
+
+        $response->assertRedirect('http://localhost:8025');
+    }
+
+    public function test_メール認証を完了すると、勤怠画面に遷移する()
+    {
+        $user = User::factory()->unverified()->create();
+
+        $verificationUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(60),
+            [
+                'id' => $user->id,
+                'hash' => sha1($user->email),
+            ]
+        );
+
+        $response = $this->actingAs($user)->get($verificationUrl);
+
+        $response->assertRedirect('/attendance');
+
+        $this->assertTrue($user->fresh()->hasVerifiedEmail());
     }
 }
--- src/tests/Feature/Auth/LoginTest.php
+++ src/tests/Feature/Auth/LoginTest.php
@@ -5,18 +5,45 @@ namespace Tests\Feature\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_メールアドレスが未入力の場合、バリデーションメッセージが表示される()
+    {
+        $response=$this->post('/login',[
+            'email'=>'',
+            'password'=>'12345678',
+        ]);
+
+        $response->assertSessionHasErrors(['email'=>'メールアドレスを入力してください']);
+    }
+
+    public function test_パスワードが未入力の場合、バリデーションメッセージが表示される()
+    {
+        $response=$this->post('/login',[
+            'email'=>'test@example.com',
+            'password'=>'',
+        ]);
+
+        $response->assertSessionHasErrors(['password'=>'パスワードを入力してください']);
+    }
+
+    public function test_登録内容と一致しない場合、バリデーションメッセージが表示される()
+    {
+        User::factory()->create([
+            'email'=>'test@example.com',
+            'password'=>bcrypt('12345678'),
+        ]);
+
+        $response=$this->post('/login',[
+            'email'=>'jest@example.com',
+            'password'=>'11111111',
+        ]);
+
+
+        $response->assertSessionHasErrors(['email'=>'ログイン情報が登録されていません']);
     }
 }
